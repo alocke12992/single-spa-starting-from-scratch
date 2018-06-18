83234e7cb961edc30fcdc1b3ca6a2c15c10132dd
alocke12992 <alocke12992@gmail.com>
2018-06-18 16:42:16 -0600

    refactor home component to show app obj

--- single-spa.config.js
+++ single-spa.config.js
@@ -2,7 +2,7 @@
 import { registerApplication, start } from 'single-spa';
 
 registerApplication('navBar', () => import('./src/navBar/navBar.app.js'), () => true);
-registerApplication('home', () => import('./src/home/home.app.js'), () => location.pathname === "" || location.pathname === "/");
+registerApplication('home', () => import('./src/home/home.app.js').then(mod => mod.homeApp), () => location.pathname === "" || location.pathname === "/");
 registerApplication('angular1', () => import('./src/angular1/angular1.app.js'), pathPrefix('/angular1'));
 
 start();
--- src/home/home.app.js
+++ src/home/home.app.js
@@ -4,28 +4,15 @@ import ReactDOM from 'react-dom';
 import singleSpaReact from 'single-spa-react';
 import Home from './root.component.js';
 
-
-
-const reactLifecycles = singleSpaReact({
+export const homeApp = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: Home,
   domElementGetter,
 })
 
-export const bootstrap = [
-  reactLifecycles.bootstrap,
-];
-
-export const mount = [
-  reactLifecycles.mount,
-];
-
-export const unmount = [
-  reactLifecycles.unmount,
-];
-
 // Finally, we specify the location we want single-spa to mount our application
 function domElementGetter() {
   return document.getElementById("home")
 }
+
