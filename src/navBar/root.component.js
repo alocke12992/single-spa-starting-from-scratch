b8482fcdd571137095e80ac8ff7f5200caed76e5
alocke12992 <alocke12992@gmail.com>
2018-06-25 14:11:21 -0600

    Refactor nav links

--- src/navBar/root.component.js
+++ src/navBar/root.component.js
@@ -4,11 +4,11 @@ import { navigateToUrl } from 'single-spa'
 const NavBar = () => (
   <nav>
     <div className="nav-wrapper">
-      <a onClick={() => navigateToUrl('/')} className="brand-logo">single-spa</a>
+      <a href="/" onClick={navigateToUrl} className="brand-logo">single-spa</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a onClick={() => navigateToUrl('/')}>Home</a></li>
+        <li><a href="/" onClick={navigateToUrl}>Home</a></li>
         {/* Note that we still need to build our AngularJS App before this link will work */}
-        <li><a onClick={() => navigateToUrl('/angular1')}>Angular1</a></li>
+        <li><a href="/angular1" onClick={navigateToUrl}>Angular1</a></li>
       </ul>
     </div>
   </nav>
