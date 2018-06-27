bc0e123b3392837ff555fdde849bce1aac14cb14
alocke12992 <alocke12992@gmail.com>
2018-06-27 13:57:25 -0600

    Refactor home pathPrefix

--- single-spa.config.js
+++ single-spa.config.js
@@ -2,7 +2,7 @@
 import { registerApplication, start } from 'single-spa';
 
 registerApplication('navBar', () => import('./src/navBar/navBar.app.js').then(mod => mod.navBar), () => true);
-registerApplication('home', () => import('./src/home/home.app.js'), (location) => location.pathname === "" || location.pathname === "/" || pathPrefix('/home')(location));
+registerApplication('home', () => import('./src/home/home.app.js'), (location) => location.pathname === "" || location.pathname === "/" || location.pathname.startsWith('/home'));
 registerApplication('angular1', () => import('./src/angular1/angular1.app.js'), pathPrefix('/angular1'));
 
 
